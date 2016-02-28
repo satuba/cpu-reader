63c298a75c22693edff5600d79d1d9eeb8de710b
azure provisioned user <super@superVM.nzg4bet2uijepadnd2w1eiwe3h.bx.internal.cloudapp.net>
2016-02-28 01:07:29 +0000

    update server and html

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 var http = require('http')
 var port = process.env.PORT || 3000;
 var fs = require('fs');
-var os = require('os');
 
-var info = os.cpus();
+var obj;
 
-fs.readFile('./index.html', function(err, html) {
-        if(err){
-          throw err;
-        }
-        http.createServer(function(req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(html);
-        res.end();
-        }).listen(port, function(){
-          console.log('server running on port '+ port);
-          console.log('cpu info: ', info);
-        });
+fs.readFile('./result.json', 'utf8', function(err, data) {
+  if(err) throw err;
+  obj = JSON.parse(data);
+  fs.writeFile('./index.html', obj.meminfo.Active.size, function (err) {
+    if (err) throw err;
+    console.log('The data was appended to file.');
+    fs.readFile('./index.html', function(err, html) {
+      if(err) throw err;
+      http.createServer(function(req, res) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(html);
+      res.end();
+      }).listen(port, function(){
+        console.log('server running on port '+ port);
+        console.log('cpu info: ', obj.meminfo.Active);
+      });
+    });
+  });
 });
+
