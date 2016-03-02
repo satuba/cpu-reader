f59b1cc0de10183a421f6abf89eb2a14e21e761e
Satu Bailey (InConsulting Inc.) <v-satbai@microsoft.com>
2016-03-02 09:51:15 -0800

    clean up

--- app/data-visualization.js
+++ app/data-visualization.js
@@ -1,25 +1,25 @@
 var dataHistory = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-var temporaryCounter = 0;
-var counter = 0;
 
 function visualize(json) {
   var percentage = 100-(json.meminfo.MemFree.size/json.meminfo.MemTotal.size*100);
   dataHistory.shift();
-  dataHistory.push(percentage);
+  //dataHistory.push(percentage);
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  var random = getRandomInt(0, 100);
+  dataHistory.push(random);
 
-  $('#memory').html(percentage.toFixed(2));
-
-  temporaryCounter += 1;  
-  counter += 1; 
+  //$('#memory').html(percentage.toFixed(2));
+  $('#memory').html(random);
 
   d3.select('#graph').selectAll("svg").remove();
-  var m = [20, 20, 20, 20]; // margins
-  var w = 500 - m[1] - m[3]; // width
-  var h = 400 - m[0] - m[2]; // height
+  var margin = [20, 20, 20, 20];
+  var width = 500 - margin[1] - margin[3];
+  var height = 400 - margin[0] - margin[2];
   
-  var data = dataHistory;
-  var x = d3.scale.linear().domain([0, 20]).range([0, w]);
-  var y = d3.scale.linear().domain([0, 100]).range([h, 0]);
+  var x = d3.scale.linear().domain([0, 20]).range([0, width]);
+  var y = d3.scale.linear().domain([0, 100]).range([height, 0]);
 
   var line = d3.svg.line()
     .x(function(d,i) { 
@@ -27,42 +27,38 @@ function visualize(json) {
     })
     .y(function(d) { 
       return y(d); 
-    })
-    var graph = d3.select("#graph").append("svg:svg")
-          .attr("width", w + m[1] + m[3])
-          .attr("height", h + m[0] + m[2])
-          .append("svg:g")
-          .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+    });
+  var graph = d3.select("#graph").append("svg:svg")
+    .attr("width", width + margin[1] + margin[3])
+    .attr("height", height + margin[0] + margin[2])
+    .append("svg:g")
+    .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
 
-    var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-    graph.append("svg:g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + h + ")")
-          .call(xAxis);
+  var xAxis = d3.svg.axis().scale(x).tickSize(-height).tickSubdivide(true);
+  graph.append("svg:g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
-    var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-    graph.append("svg:g")
-          .attr("class", "y axis")
-          .attr("transform", "translate(-25,0)")
-          .call(yAxisLeft);
-  
-    graph.append("svg:path").attr("d", line(data));
-    console.log(dataHistory);
+  var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+  graph.append("svg:g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(-25,0)")
+    .call(yAxisLeft);
+
+  graph.append("svg:path").attr("d", line(dataHistory));
 }
 
-function ajaxCall() {
+setInterval(function(){
   $.ajax({
     url: "/data",
     type : 'GET',
     dataType : 'json',
     success: function (jsonData) {
       visualize(jsonData);
-      addValues(jsonData);
     },
     error: function(err){
-      console.log(err);
+      return console.log(err);
     }
   });
-}
-
-setInterval(ajaxCall, 1000);
+}, 1000);
