d232e9352deb8a587a82651d485afed2338d8521
Satu Bailey (InConsulting Inc.) <v-satbai@microsoft.com>
2016-03-02 15:01:34 -0800

    modify

--- routes/routes.js
+++ routes/routes.js
@@ -4,20 +4,16 @@ var bodyparser = require("body-parser");
 var fs = require("fs");
 var child_process = require('child_process');
 
-function updateData(){
+module.exports = function(router) {
   child_process.exec('luajit jsonfythis.lua meminfo', function(error, stdout, stderr){
     if(error) throw error;
-  });
-}
-
-module.exports = function(router) {
-  router.use(bodyparser.json());
-  router.get("/data", function(req, res) {
-    fs.readFile("./data.json", function(err, data) {
-      if (err) {console.log(err);}
-      var fileContent = JSON.parse(data.toString());
-      res.json(fileContent); 
-      updateData();
+    router.use(bodyparser.json());
+    router.get("/data", function(req, res) {
+      fs.readFile("./data.json", function(err, data) {
+        if (err) {console.log(err);}
+        var fileContent = JSON.parse(data.toString());
+        res.json(fileContent);
+      });
     });
   });
 };
