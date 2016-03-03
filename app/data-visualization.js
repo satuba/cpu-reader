76f22e4910b76a1f07f3d75271245b6a16f22366
Satu Bailey <satu.bailey@gmail.com>
2016-03-02 20:07:28 -0800

    add cpu

--- app/data-visualization.js
+++ app/data-visualization.js
@@ -1,34 +1,23 @@
-var dataHistory = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var ramHistory = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var cpuHistory = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-function visualize(json) {
-  var percentage = 100-(json.meminfo.MemFree.size/json.meminfo.MemTotal.size*100);
-  dataHistory.shift();
-  dataHistory.push(percentage);
-  // function getRandomInt(min, max) {
-  //   return Math.floor(Math.random() * (max - min)) + min;
-  // }
-  // var random = getRandomInt(0, 100);
-  // dataHistory.push(random);
-
-  $('#memory').html(percentage.toFixed(4));
-  //$('#memory').html(random);
-
-  d3.select('#graph').selectAll("svg").remove();
+function visualize(dataArray, svgGraph) {
+  d3.select(svgGraph).selectAll("svg").remove();
   var margin = [20, 20, 20, 20];
   var width = 500 - margin[1] - margin[3];
   var height = 400 - margin[0] - margin[2];
-  
+
   var x = d3.scale.linear().domain([0, 20]).range([0, width]);
   var y = d3.scale.linear().domain([0, 100]).range([height, 0]);
 
   var line = d3.svg.line()
-    .x(function(d,i) { 
-      return x(i); 
+    .x(function(d,i) {
+      return x(i);
     })
-    .y(function(d) { 
-      return y(d); 
+    .y(function(d) {
+      return y(d);
     });
-  var graph = d3.select("#graph").append("svg:svg")
+  var graph = d3.select(svgGraph).append("svg:svg")
     .attr("width", width + margin[1] + margin[3])
     .attr("height", height + margin[0] + margin[2])
     .append("svg:g")
@@ -46,19 +35,52 @@ function visualize(json) {
     .attr("transform", "translate(-25,0)")
     .call(yAxisLeft);
 
-  graph.append("svg:path").attr("d", line(dataHistory));
+  graph.append("svg:path").attr("d", line(dataArray));
 }
 
 setInterval(function(){
   $.ajax({
-    url: "/data",
+    url: "/ram",
     type : 'GET',
     dataType : 'json',
     success: function (jsonData) {
-      visualize(jsonData);
+      var percentage = 100-(jsonData.meminfo.MemFree.size/jsonData.meminfo.MemTotal.size*100);
+      $('#ram').html(percentage.toFixed(4));
+      ramHistory.shift();
+      ramHistory.push(percentage);
+      visualize(ramHistory, "#ramGraph");
+    },
+    error: function(err){
+      return console.log(err);
+    }
+  });
+
+  $.ajax({
+    url: "/cpu",
+    type : 'GET',
+    success: function (data) {
+      $('#cpu').html(data.cpu.toFixed(4));
+      $('#model').html(data.model);
+      cpuHistory.shift();
+      cpuHistory.push(data.cpu);
+      visualize(cpuHistory, "#cpuGraph");
     },
     error: function(err){
       return console.log(err);
     }
   });
 }, 1000);
+
+$(document).ready(function(){
+  $('#ramGraph').hide();
+
+  $('#ramButton').click(function() {
+    $('#ramGraph').show();
+    $('#cpuGraph').hide();
+  });
+
+  $('#cpuButton').click(function() {
+    $('#ramGraph').hide();
+    $('#cpuGraph').show();
+  });
+});
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ var express = require("express");
 var bodyparser = require("body-parser");
 var fs = require("fs");
 var child_process = require('child_process');
+var os = require("os");
 
 function updateData(){
   child_process.exec('luajit jsonfythis.lua meminfo', function(error, stdout, stderr){
@@ -10,14 +11,41 @@ function updateData(){
   });
 }
 
+function calculateCPU(){
+  var cpus = os.cpus();
+  var totalIdle = 0;
+  var totalTick = 0;
+  var model = "";
+
+  for(var i = 0; i < cpus.length; i++) {
+    var cpu = cpus[i];
+    for(var type in cpu.times) {
+      totalTick += cpu.times[type];
+   }
+    totalIdle += cpu.times.idle;
+    model = cpu.model;
+  }
+
+  totalIdle = totalIdle / cpus.length;
+  totalTick = totalTick / cpus.length;
+
+  return {cpu: 100-(totalIdle/totalTick*100), model: model};
+}
+
 module.exports = function(router) {
   router.use(bodyparser.json());
-  router.get("/data", function(req, res) {
+
+  router.get("/ram", function(req, res) {
     fs.readFile("./data.json", function(err, data) {
       if (err) {console.log(err);}
       var fileContent = JSON.parse(data.toString());
-      res.json(fileContent); 
+      res.json(fileContent);
       updateData();
     });
+  });
+
+  router.get("/cpu", function(req, res){
+    var data = calculateCPU()
+    res.json(data);
   });
 };
